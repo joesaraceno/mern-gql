ac7f74c98b2bc5bbfd5f41e5075d47557c21278c
joesaraceno <josephasaraceno@gmail.com>
2019-08-08 15:24:18 -0600

    mutations and types queries

--- app.js
+++ app.js
@@ -5,46 +5,69 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!")
-});
-
 app.use(bodyParser.json());
 
-
-const eventResponse = [
-    'sailing',
-    'reading',
-    'meetup',
-    'hacking'
-]
+const events = [
+    
+];
 
 app.use(
     "/graphql",
     graphqlHttp({
         schema: buildSchema (`
+            type Event {
+                _id: ID!,
+                title: String!,
+                description: String!,
+                cost: Float!,
+                start_time: String!,
+                end_time: String!,
+            },
+
+            input EventInput {
+                title: String!,
+                description: String!,
+                cost: Float!,
+                start_time: String!,
+                end_time: String!,
+            },
+
             type RootQuery {
-                events: [String!]!  
+                events: [Event!]! 
             },
+
             type RootMutation {
-                createEvent(name: String): String
+                createEvent(event: EventInput): Event
             },
+
             schema {
                 query: RootQuery,
                 mutation: RootMutation
-            }
+            },
         `),
         rootValue: {
             events: () => {
-                return eventResponse;
+                return events;
             },
-            createEvent: (args) => {
-                const {name} = args;
-                return name;
+            createEvent: ({event}) => {
+                const input = {
+                    _id: getRandomArbitrary(1, 2).toString(),
+                    title: event.title,
+                    cost: event.cost,
+                    start_time: event.start_time,
+                    end_time: event.end_time,
+                    description: event.description 
+                };
+                events.push(input);
+                return input;
             },
         },
         graphiql: true,
     })
 );
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
 
 app.listen("44441");
